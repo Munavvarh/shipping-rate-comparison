b68afbb80b14aa13cb6767db1ab6ae4b301c0974
Sahrish Afzal <sahrish.afzal0987@gmail.com>
2024-06-21 15:44:44 -0400

    Added header section

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+function Header() {
+    return (
+        <div className="header">
+            
+        </div>
+    );
+}
+
+ReactDOM.render(<Header />, document.getElementById('root'));
+
